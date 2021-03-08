f7a50b87604c268f22c0384f15f151110f486451
unknown <jpaulogomes@ipvc.pt>
2021-03-08 17:14:17 +0000

    Aula3_Exercicio4

---
+++ Aula 3/exercicio4/script.js
@@ -0,0 +1,18 @@
+// Your JS Script here
+
+let imgCounter = 0;
+
+function addImage (){
+
+    imgCounter++;
+    let img = document.createElement("img")
+    img.src="https://placeing.com/250/150/" + imgCounter
+
+    const images = document.getElementById("imageContainer")
+    images.appendChild(img)
+}
+
+function delImage () {
+    const images = document.getElementById("imageContainer")
+    images.children[0].remove()
+}
